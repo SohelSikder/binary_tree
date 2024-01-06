0955eabdb2b0b42903eb9c9a9268d8cc10c882d0
SohelSikder <cse.sohelsikder@gamil.com>
2024-01-06 11:23:07 +0600

    added levelOrderPrint

--- binary_tree_inorder.cpp
+++ binary_tree_inorder.cpp
@@ -1,43 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 class Node {
     public:
         int val;
-        Node * left;
-        Node * right;
-    Node( int val){
+        Node * left ;
+        Node * right ;
+    Node (int val){
         this->val = val;
-        this->right = NULL;
         this->left = NULL;
-    }
+        this->right = NULL;
+    } 
 };
-void preorder(Node * root){
+void inorder(Node * root){
     if(root==NULL){
         return ;
-
     }
-    
-    preorder(root->left);
-    cout<<root->val <<" ";
-    preorder(root->right);
-    
+    inorder(root->left);
+    cout<< root->val <<" ";
+    inorder(root->right);
+
 }
 int main(){
-
-    Node * root = new Node(10);
-    Node * a = new Node(20);
-    Node * b = new Node (30);
-    Node * c = new Node (40);
-    Node * d = new Node (50);
-    Node * e = new Node (60);
-    Node * f = new Node (70);
-    Node * g = new Node (80);
-    Node * h = new Node (90);
-    Node * i = new Node (100);
-
-    // connection ;
-
+    Node * root = new Node(5);
+    Node * a = new Node(10);
+    Node * b = new Node(20);
+    Node * c = new Node(30);
+    Node * d = new Node(40);
+    Node * e = new Node(50);
+    Node * f = new Node(60);
+    Node * g = new Node(70);
+    Node * h = new Node(80);
+    Node * i = new Node(90);
+    
+    // connection 
     root->left= a ;
     root->right = b;
     a->left = c;
@@ -47,7 +42,7 @@ int main(){
     d->left = f;
     d->right = g;
     h->right = i;
-    preorder(root);
+    inorder(root);
     return 0;
 }
 
